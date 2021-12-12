475abb43a6089ba05d92eed0c072b5fcfde0b4b0
kaiappelt <kaiappelt53@gmail.com>
2021-12-12 14:41:57 -0300

    Configuração do cors

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import routes from './routes';
 import cors from 'cors';
 
 let app = express();
-app.use(express.json());
 app.use(cors());
-  
+app.use(express.json());
+
 app.use(routes);
 
 // Habilita o retorno de erros do celebrate
