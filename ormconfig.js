051f3796994af24fa764e82fc766e4866dbfa426
kaiappelt <kaiappelt53@gmail.com>
2022-01-09 17:56:58 -0300

    Ajuste na rota raíz da api

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+require('dotenv/config');
+
+module.exports = {
+    type: "postgres",
+    url: "postgres://ubbpdsxrkmqpnh:6f820abb5284cca65a16ab5e624b00f605271c9d5cc543533c15453a43f2c1b3@ec2-44-198-214-172.compute-1.amazonaws.com:5432/d61qq2n55fgocc",
+
+    entities: ["./dist/modules/**/entities/*.js"],
+    migrations: ["./dist/shared/infra/typeorm/migrations/*.js"],
+    cli: {
+      "migrationsDir": "./dist/shared/infra/typeorm/migrations"
+    },
+    synchronize: false,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
+};
