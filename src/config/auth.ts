00c1f9e8cca889e2eae1eda8e9a01e8a50238735
kaiappelt <kaiappelt53@gmail.com>
2022-01-22 17:43:37 -0300

    Arquivo .env

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,8 @@
+require("dotenv/config");
+
 export default {
     jwt: {
-        secret: "f8b47d2e95c114112d9cc2158adeb774",
-        expiresIn: "1d",
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES,
     }
 }
