f580d7b66802ec36bfd2eb60e81e57158b5563da
kaiappelt <kaiappelt53@gmail.com>
2022-02-03 23:34:33 -0300

    Ajuste na configuração do Redis

--- dist/config/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-exports.default = {
-    jwt: {
-        secret: process.env.JWT_SECRET,
-        expiresIn: process.env.JWT_EXPIRES,
-    }
-};
